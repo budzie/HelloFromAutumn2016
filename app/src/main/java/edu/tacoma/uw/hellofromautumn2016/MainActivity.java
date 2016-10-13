1076995ad300625fe8154d1f010890ab47fd543f
budzie <sb15@uw.edu>
2016-10-12 17:42:55 -0700

    Initial commit

---
+++ app/src/main/java/edu/tacoma/uw/hellofromautumn2016/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.hellofromautumn2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /** main*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
